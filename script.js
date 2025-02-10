abc932a09ffd0c7f8d00dff566b02061b11213ca
Omm Prakash Samal <ommprakashsamal9@gmail.com>
2025-02-10 23:38:21 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const url = 'https://api.openweathermap.org/data/2.5/weather';
-const apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
+const url = {ApiURL};
+const apiKey = {ApiKey};
+// const url = 'https://api.openweathermap.org/data/2.5/weather';
+// const apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
+
 let lastWeatherCondition = 'clear'; // Store last weather condition
 
 // Default city on load
